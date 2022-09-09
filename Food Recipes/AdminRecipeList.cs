11e45c31545cf13de98ec7df676329f99fdc9fb9
jayani1996 <pgtjayani@gmail.com>
2022-09-09 17:51:39 +0530

    First Commit

---
+++ Food Recipes/AdminRecipeList.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Food_Recipes
+{
+    public partial class AdminRecipeList : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-AM7OELU\\MYSQLSERVER;Initial Catalog=FoodRecipes;Integrated Security=True");
+        SqlDataAdapter da = new SqlDataAdapter();
+
+        public static string SetValueForRecipeId;
+        string recipeId;
+        string catId;
+
+        public AdminRecipeList()
+        {
+            InitializeComponent();
+        }
+
+        private void AdminRecipeList_Load(object sender, EventArgs e)
+        {
+            loadGridViewData();
+            this.dgvRecipes.Columns[0].Visible = false;
+        }
+
+        private void loadGridViewData()
+        {
+            da.SelectCommand = new SqlCommand("SELECT R.RecipeID as [Recipe ID], R.RecipeName as [Recipe Name], R.Descriptions as [Description], R.Duration as [Duration], C.RecipeCategoryName as [Category], R.Image as [Image] FROM Recipes R JOIN RecipeCategories C ON(R.RecipeCategoryID = C.RecipeCategoryID)", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            this.dgvRecipes.DefaultCellStyle.Font = new Font(" Microsoft JhengHei", 11);
+            //Count Number of Recipies
+            dgvRecipes.DataSource = dt;
+            dataGrideViewRowCount();
+
+            //make row  height 
+            dgvRecipes.RowTemplate.Height = 100;
+            //dgvCategory.AllowUserToAddRows = false;
+
+            dgvRecipes.DataSource = dt;
+
+            //stretch image in colum
+            DataGridViewImageColumn imgCol = new DataGridViewImageColumn();
+            imgCol = (DataGridViewImageColumn)dgvRecipes.Columns[5];
+            imgCol.ImageLayout = DataGridViewImageCellLayout.Stretch;
+
+            //auto make size of column 
+            dgvRecipes.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+
+        }
+
+        private void dgvRecipes_Click(object sender, EventArgs e)
+        {
+            //get recipe if from selected row
+            recipeId = dgvRecipes.CurrentRow.Cells[0].Value.ToString();
+            SetValueForRecipeId = recipeId;
+
+
+        }
+
+        private void btnAddRecipe_Click(object sender, EventArgs e)
+        {
+            AdminAddRecipe add = new AdminAddRecipe();
+            add.MdiParent = this.ParentForm;
+            add.Dock = DockStyle.Fill;
+            add.Show();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (recipeId != null)
+            {
+                //Pass selected value to Update Form
+                SetValueForRecipeId = recipeId;
+
+                AdminRecipeUpdateDelete update = new AdminRecipeUpdateDelete();
+                update.MdiParent = this.ParentForm;
+                update.Dock = DockStyle.Fill;
+                update.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Recipe!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        //Count Number of Recipies
+        public void dataGrideViewRowCount()
+        {
+            int rowcount = dgvRecipes.Rows.Count;
+            lblNumOfRecipies.Text = rowcount.ToString();
+        }
+
+        private void panelContainer_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //search category from table
+            string searchQuery = "";
+            if (textBox1.Text != string.Empty)
+                searchQuery += "[Recipe Name] like '%" + textBox1.Text.Trim() + "%' and ";
+            searchQuery += "1 = 1";
+            (dgvRecipes.DataSource as DataTable).DefaultView.RowFilter = searchQuery;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            AdminDashboard list = new AdminDashboard();
+            list.MdiParent = this.ParentForm;
+            list.Dock = DockStyle.Fill;
+            list.Show();
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            loadGridViewData();
+        }
+    }
+}
---
+++ Food Recipes/SQLCONNECTION.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Food_Recipes
+{
+    class SQLCONNECTION
+    {
+        public SqlConnection cs;
+
+        public void Connection()
+        {
+            cs = new SqlConnection("Data Source=DESKTOP-AM7OELU\\MYSQLSERVER;Initial Catalog=FoodRecipes;Integrated Security=True");
+        }
+    }
+}
